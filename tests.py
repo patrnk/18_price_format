8a26bba065fba88c1f060f7be8387fed00a23447
Paul Vergeev <paulvergeev@gmail.com>
2017-08-05 14:20:44 +0300

    Make tests more atomic

--- tests.py
+++ tests.py
@@ -6,20 +6,47 @@ import format_price
 
 class FormatCorrectPriceTestCase(unittest.TestCase):
 
-    def run_test(self, inputs, expected_outputs):
-        for input_index, input_value in enumerate(inputs):
-            output = format_price.format_price(input_value)
-            self.assertEqual(output, expected_outputs[input_index])
-    
-    def test_format_price_formats_integers(self):
-        inputs = ['5.000000', '3245.000000', '3003245.000000']
-        expected_outputs = ['5', '3 245', '3 003 245']
-        self.run_test(inputs, expected_outputs)
+    def test_format_price_formats_single_digit_integer(self):
+        test_input = '5.000000'
+        expected_output = '5'
+        output = format_price.format_price(test_input)
+        self.assertEqual(output, expected_output)
 
-    def test_format_price_formats_floats(self):
-        inputs = ['0.000000', '0.000001', '5.000001', '3245.003000', '3003245.100000']
-        expected_outputs = ['0', '5,000001', '3 245,003', '3 003 245,1']
-        self.run_test(inputs, expected_outputs)
+    def test_format_price_formats_thousands_value_integer(self):
+        test_input = '3245.000000'
+        expected_output = '3 245'
+        output = format_price.format_price(test_input)
+        self.assertEqual(output, expected_output)
+
+    def test_format_price_formats_milions_value_integer(self):
+        test_input = '3003245.000000'
+        expected_output = '3 003 245'
+        output = format_price.format_price(test_input)
+        self.assertEqual(output, expected_output)
+
+    def test_format_price_formats_zero(self):
+        test_input = '0.000000'
+        expected_output = '0'
+        output = format_price.format_price(test_input)
+        self.assertEqual(output, expected_output)
+
+    def test_format_price_formats_one_digit_float(self):
+        test_input = '0.000001'
+        expected_output = '0,000001'
+        output = format_price.format_price(test_input)
+        self.assertEqual(output, expected_output)
+
+    def test_format_price_formats_float_with_leading_and_trailing_zeros(self):
+        test_input = '0.003000'
+        expected_output = '0,003'
+        output = format_price.format_price(test_input)
+        self.assertEqual(output, expected_output)
+
+    def test_format_price_formats_multiple_digit_float(self):
+        test_input = '0.123000'
+        expected_output = '0,123'
+        output = format_price.format_price(test_input)
+        self.assertEqual(output, expected_output)
 
 
 class FormatIncorrectPriceTestCase(unittest.TestCase):
@@ -41,21 +68,70 @@ class FormatIncorrectPriceTestCase(unittest.TestCase):
 
 class IsCorrectlyFormattedTestCase(unittest.TestCase):
 
-    def run_test(self, inputs, expected_output):
-        for input_index, input_value in enumerate(inputs):
-            output = format_price.is_correctly_formatted(input_value)
-            self.assertEqual(output, expected_output)
+    def test_is_correctly_formatted_returns_true_for_zero(self):
+        test_input = '0.000000'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertTrue(output)
 
-    def test_is_correctly_formatted_returns_true(self):
-        inputs = ['0.000000', '0.000001', '5.000000', 
-                  '3245.001000', '3003245.999999']
-        self.run_test(inputs, True)
+    def test_is_correctly_formatted_returns_true_for_typical_float(self):
+        test_input = '3245.001200'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertTrue(output)
 
-    def test_is_correctly_formatted_returns_false(self):
-        inputs = ['0005.000000', '3245', '-3003245.999999',
-                  'Agent 007', 'NaN', '123.456', '123.E4',
-                  '.1', '+5.000000', 'infinity','53.', '1e1']
-        self.run_test(inputs, False)
+    def test_is_correctly_formatted_returns_true_for_integer(self):
+        test_input = '123.000000'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertTrue(output)
+
+    def test_is_correctly_formatted_returns_false_for_leading_zeros(self):
+        test_input = '0005.000000'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertFalse(output)
+
+    def test_is_correctly_formatted_returns_false_for_integer_without_decimal_point(self):
+        test_input = '3245'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertFalse(output)
+
+    def test_is_correctly_formatted_returns_false_for_negative_float(self):
+        test_input = '-123.100000'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertFalse(output)
+
+    def test_is_correctly_formatted_returns_false_for_arbitrary_string(self):
+        test_input = 'Agent 007'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertFalse(output)
+
+    def test_is_correctly_formatted_returns_false_for_nan(self):
+        test_input = 'NaN'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertFalse(output)
+
+    def test_is_correctly_formatted_returns_false_for_fraction_in_scientific_notation(self):
+        test_input = '123.E4'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertFalse(output)
+
+    def test_is_correctly_formatted_returns_false_for_fraction_without_integer_part(self):
+        test_input = '.1'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertFalse(output)
+
+    def test_is_correctly_formatted_returns_false_for_plus_sign(self):
+        test_input = '+5.000000'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertFalse(output)
+
+    def test_is_correctly_formatted_returns_false_for_integer_without_fraction_part(self):
+        test_input = '53.'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertFalse(output)
+
+    def test_is_correctly_formatted_returns_false_for_integer_in_scientific_notation(self):
+        test_input = '1e1'
+        output = format_price.is_correctly_formatted(test_input)
+        self.assertFalse(output)
 
 
 if __name__ == '__main__':
